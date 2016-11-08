b3062cb24d0d2988e696c33156f8db8e3effa045
FELIPE RONDEROS <feliperonderos@gmail.com>
2016-11-07 20:36:18 -0600

    added everything

---
+++ prog2.Csharp/Tree/BuiltIn.cs
@@ -0,0 +1,128 @@
+// BuiltIn -- the data structure for built-in functions
+
+// Class BuiltIn is used for representing the value of built-in functions
+// such as +.  Populate the initial environment with
+// (name, new BuiltIn(name)) pairs.
+
+// The object-oriented style for implementing built-in functions would be
+// to include the C# methods for implementing a Scheme built-in in the
+// BuiltIn object.  This could be done by writing one subclass of class
+// BuiltIn for each built-in function and implementing the method apply
+// appropriately.  This requires a large number of classes, though.
+// Another alternative is to program BuiltIn.apply() in a functional
+// style by writing a large if-then-else chain that tests the name of
+// the function symbol.
+
+using System;
+
+namespace Tree
+{
+    public class BuiltIn : Node
+    {
+        private string symbol;            // the Ident for the built-in function
+
+        public BuiltIn(string s)		{ symbol = s; }
+
+        //public Node getSymbol()		{ return symbol; }
+
+        // TODO: The method isProcedure() should be defined in
+        // class Node to return false.
+        public override bool isProcedure()	{ return true; }
+
+        public override void print(int n)
+        {
+            // there got to be a more efficient way to print n spaces
+            for (int i = 0; i < n; i++)
+                Console.Write(' ');
+            Console.Write("#{Built-in Procedure ");
+            //if (symbol != null)
+            //    symbol.print(-Math.Abs(n));
+            Console.Write('}');
+            if (n >= 0)
+                Console.WriteLine();
+        }
+
+        // TODO: Implement method apply()
+        // It should be overridden only in classes BuiltIn and Closure
+        public override Node apply(Node args, Environment env)
+        {
+            string s = symbol;
+            Node arg1 = args.getCar();
+            Node arg2 = (!args.getCdr().isNull()) ? (args.getCdr().getCar()) : null ;
+            switch (s)
+            {
+                case "+":
+                    return new IntLit((int)arg1.eval(env) + (int)arg2.eval(env));
+                    break;
+                case "-":
+                    return new IntLit((int)arg1.eval(env) - (int)arg2.eval(env));
+                    break;
+                case "*":
+                    return new IntLit((int)arg1.eval(env) * (int)arg2.eval(env));
+                    break;
+                case "/":
+                    return new IntLit((int)arg1.eval(env) / (int)arg2.eval(env));
+                    break;
+                case "=":
+                    return new BoolLit((int)arg1.eval(env) == (int)arg2.eval(env));
+                    break;
+                case ">":
+                    return new BoolLit((int)arg1.eval(env) > (int)arg2.eval(env));
+                    break;
+                case ">=":
+                    return new BoolLit((int)arg1.eval(env) >= (int)arg2.eval(env));
+                    break;
+                case "<=":
+                    return new BoolLit((int)arg1.eval(env) <= (int)arg2.eval(env));
+                    break;
+                case "<":
+                    return new BoolLit((int)arg1.eval(env) < (int)arg2.eval(env));
+                    break;
+
+                case "symbol?":
+                    return new BoolLit(arg1.isSymbol());
+                    break;
+                case "number?":
+                    return new BoolLit(arg1.isNumber());
+                    break;
+                case "procedure?":
+                    return new BoolLit(arg1.isProcedure());
+                    break;
+                case "car":
+                    return arg1;
+                    break;
+                case "cdr":
+                    return args.getCdr();
+                    break;
+                case "cons":
+                    return new Cons((Node)args.GetCar(),args.getCdr());
+                    break;
+                case "set-car!":
+                    return new Cons(arg2, arg1.getCdr());
+                    break;
+                case "set-cdr!":
+                    return new Cons(arg1.getCar(),arg2);
+                    break;
+                case "null?":
+                    return new BoolLit(arg1.isNull());
+                    break;
+                case "pair?":
+                    return new BoolLit(arg1.isPair());
+                    break;
+                case "eq?":
+                    return new BoolLit(arg1.eval(env) == arg2.eval(env));
+                    break;
+                default:
+                    return null;
+                /*
+                 
+                 */
+            }
+
+
+            //return new StringLit("Error: BuiltIn.apply not yet implemented");
+
+        }
+    }    
+}
+
---
+++ prog2.Csharp/Tree/Cons.cs
@@ -0,0 +1,150 @@
+// Cons -- Parse tree node class for representing a Cons node
+
+using System;
+
+namespace Tree
+{
+    public class Cons : Node
+    {
+        private Node car;
+        private Node cdr;
+        private Special form;
+    
+        public Cons(Node a, Node d)
+        {
+            car = a;
+            cdr = d;
+            parseList();
+        }
+        public override bool isPair() { return true; }  // Cons
+
+        // parseList() `parses' special forms, constructs an appropriate
+        // object of a subclass of Special, and stores a pointer to that
+        // object in variable form.  It would be possible to fully parse
+        // special forms at this point.  Since this causes complications
+        // when using (incorrect) programs as data, it is easiest to let
+        // parseList only look at the car for selecting the appropriate
+        // object from the Special hierarchy and to leave the rest of
+        // parsing up to the interpreter.
+        private void parseList()
+        {
+            if (car.isSymbol())
+            {
+                Ident special = (Ident)car;
+                switch (special.getName())
+                {
+                    case "quote":
+                        form = new Quote();
+                        break;
+                    case "lambda":
+                        form = new Lambda();
+                        break;
+                    case "begin":
+                        form = new Begin();
+                        break;
+                    case "if":
+                        form = new If();
+                        break;
+                    case "let":
+                        form = new Let();
+                        break;
+                    case "cond":
+                        form = new Cond();
+                        break;
+                    case "define":
+                        form = new Define();
+                        break;
+                    case "set!":
+                        form = new Set();
+                        break;
+                    default:
+                        form = new Regular();
+                        break;
+                }
+            }
+            else form = new Regular();
+        }
+ 
+        public override void print(int n)
+        {
+            form.print(this, n, 0, false,1);
+        }
+        
+        public override void print(int n, int indentation, bool p, int state)
+        {
+            form.print(this, n, indentation, p, state);
+        }
+        
+        public override Node getCar()
+        {
+            return car;
+        }
+
+        public override Node getCdr()
+        {
+            return cdr;
+        }
+        public override object eval(Environment e)
+        {
+            if (form is If)
+            {
+                if ((Boolean)cdr.getCar().eval(e))
+                {
+                    return cdr.getCdr().getCar().eval(e);
+                }
+                else
+                {
+                    return cdr.getCdr().getCdr().getCar().eval(e);
+                }
+            }
+            else if (form is Cond) {
+                Node nextClause = cdr;
+                while ((nextClause.getCar().getCar().getName() != "else") && (!((Boolean)nextClause.getCar().getCar().eval(e)))) {
+                    nextClause = nextClause.getCdr();
+                }
+                return nextClause.getCar().getCdr().getCar().eval(e);
+            }
+            //TODO: Closures
+            else if (form is Define)
+            {
+                e.define(cdr.getCar().getName(), cdr.getCdr().getCar());
+            }
+            else if (form is Lambda) {
+                ;
+            }
+            else if (form is Let)
+            {
+                ;
+            }
+            else if (form is Quote)
+            {
+                ;
+            }
+            else if (form is Begin)
+            {
+                ;
+            }
+            //END TODO
+            else if (form is Set)
+            {
+                e.change(cdr.getCar().getName(), cdr.getCdr().getCar());
+            }
+            else if ((car.eval(e) is Node) && (((Node)car.eval(e)).isProcedure()))
+            {
+                return ((Node)car.eval(e)).apply(cdr, e).eval(e);
+            }
+            return null;
+        }
+        // After setCar, a Cons cell needs to be `parsed' again using parseList.
+        public override void setCar(Node a)
+        {
+            car = a;
+            parseList();
+        }
+        public override void setCdr(Node d)
+        {
+            cdr = d;
+        }
+    }
+}
+
